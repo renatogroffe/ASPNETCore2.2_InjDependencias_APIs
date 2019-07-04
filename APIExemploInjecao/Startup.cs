51d3c90d9d4ba6e1a1e2f170e05f22644a0f71b8
Renato Groffe <renato.groffe@yahoo.com.br>
2019-07-04 16:34:23 -0300

    ASP.NET Core 2.2 + REST API + Injeção de Dependências
    ASP.NET Core 2.2 + REST API + Injeção de Dependências

---
+++ APIExemploInjecao/Startup.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using APIExemploInjecao.Testes;
+
+namespace APIExemploInjecao
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSingleton<ITesteA, TesteA>();
+            services.AddTransient<ITesteB, TesteB>();
+            services.AddScoped<TesteC>();
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseMvc();
+        }
+    }
+}
